e3f92bd0d4b16450525e56a7397962b5f0c86135
Tom <micathom@gmail.com>
2012-12-13 17:42:41 +0100

    r.tskel: tests/test_*.R: set  test_that context()

--- templator/r.tskel/tests/test_${1=main}.R
+++ templator/r.tskel/tests/test_${1=main}.R
@@ -3,6 +3,8 @@ test.<+call:get(b:templator_args, "1", "main")+> <- function() {
     <+CURSOR+>
 }
 <+else+>
+context("<+call:get(b:templator_args, "1", "main")+>")
+
 test_that("<+call:get(b:templator_args, "1", "main")+>", {
     # expect_that(observed, expected)
 
